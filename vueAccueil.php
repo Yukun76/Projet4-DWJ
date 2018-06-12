237bd3c47fdc3d08f7391fe344c8d586ccaa2123
Yukun <laruejeremy@hotmail.fr>
2018-06-12 14:18:52 +0200

    Factoristation des éléments d'affichage communs

--- vueAccueil.php
+++ vueAccueil.php
@@ -1,18 +1,16 @@
-<!doctype html>
-<html lang="fr">
-<head>
-  ...
-</head>
-<body>
-  ...
-    <div id="contenu">
-      <?php foreach ($billets as $billet): ?>
-        <article>
-          ...
-        </article>
-        <hr />
-      <?php endforeach; ?> 
-    </div> <!-- #contenu -->
-  ...
-</body>
-</html>
+<?php $titre = 'Mon Blog'; ?>
+
+<?php ob_start(); ?>
+<?php foreach ($billets as $billet): ?>
+  <article>
+    <header>
+      <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+      <time><?= $billet['date'] ?></time>
+    </header>
+    <p><?= $billet['contenu'] ?></p>
+  </article>
+  <hr />
+<?php endforeach; ?>
+<?php $contenu = ob_get_clean(); ?>
+
+<?php require 'gabarit.php'; ?>

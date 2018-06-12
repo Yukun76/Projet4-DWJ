75ee6ebf1fd1464c5c61dd44b924299027d4ab7e
Yukun <laruejeremy@hotmail.fr>
2018-06-12 14:13:57 +0200

    Isolation de l'affichage

---
+++ vueAccueil.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  ...
+</head>
+<body>
+  ...
+    <div id="contenu">
+      <?php foreach ($billets as $billet): ?>
+        <article>
+          ...
+        </article>
+        <hr />
+      <?php endforeach; ?> 
+    </div> <!-- #contenu -->
+  ...
+</body>
+</html>

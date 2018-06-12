c32f814b0dcb786d1d683f3c1e52b223c06024a2
Yukun <laruejeremy@hotmail.fr>
2018-06-12 14:53:10 +0200

    Mise en oeuvre d'un controleur frontal

--- vueAccueil.php
+++ vueAccueil.php
@@ -4,8 +4,8 @@
 <?php foreach ($billets as $billet): ?>
     <article>
         <header>
-            <a href="<?= "billet.php?id=" . $billet['id'] ?>">
-                <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+            <a href="<?= "index.php?action=billet&id=" . $billet['id'] ?>">
+               <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
             </a>
             <time><?= $billet['date'] ?></time>
         </header>
